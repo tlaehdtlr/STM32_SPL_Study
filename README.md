ed2c29e48d3fa8c96475f5bb0119ebcc638bd671
tlaehdtlr <tlaehdtlr@gmail.com>
2023-02-07 13:25:09 +0900

    init uart project

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # STM32 SPL Study
 - Embitz, STM32F091RC
-  
+
+### projects
+- GPIO : LED(output), button(input, external interrupt)
+- UART : Command line interface
---
+++ UART_CLI/inc/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f0xx_conf.h"
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_H_INCLUDED */
